3d7f64de0bae9aef9587445918616e9a6000b015
Tiefengnom <111353598+Tiefengnom@users.noreply.github.com>
2023-01-12 18:33:25 +0100

    s

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { NavLink, useNavigate} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { useUserContext } from "../hooks/useUserContext"
 //
 
